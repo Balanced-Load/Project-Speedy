a6902a75c733133669258def519f5312c3933827
Trevor Leung <humormy@gmail.com>
2022-03-05 13:58:46 -0800

    fixed id issue

--- Reviews/app.test.js
+++ Reviews/app.test.js
@@ -24,3 +24,28 @@ describe("Test meta endpoint", () => {
       })
   });
 });
+
+describe("Test reviews post endpoint", () => {
+  test("POST /reviews", (done) => {
+    request(app)
+      .post("/reviews")
+      .send(
+        {
+          "product_id": 40347,
+          "rating": 5,
+          "summary": "summary",
+          "body": "body",
+          "recommend": false,
+          "name": "name",
+          "email": "email@gmail.com",
+          "photos": ["photo1", "photo2"],
+          "characteristics": {"14": 5, "15": 1}
+        }
+      )
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      })
+  });
+});
